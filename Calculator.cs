9dd3da9897584959bbdd4b158675c5ea02003edf
Александр Жаворонок <C0baltt@live.com>
2021-12-05 00:37:14 +0300

    Start of development

---
+++ Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolatzHypothesis_3xplus1
+{
+    public class Calculator
+    {
+        public static List<long> Calculation(long x)
+        {
+            List<long> hailstones = new(1);
+            long hailstone = x;
+
+            while (true)
+            {
+                if (hailstone % 2 == 0)
+                {
+                    hailstone /= 2;
+                }
+                else
+                {
+                    hailstone *= 3;
+                    hailstone += 1;
+                }
+
+                if (hailstones.Contains(hailstone))
+                {
+                    return hailstones;
+                }
+
+                hailstones.Add(hailstone);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KolatzHypothesis_3xplus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long y;
+
+            Console.Write("Input natural number: ");
+            var input = Console.ReadLine();
+
+            Int64.TryParse(input, out y);
+
+            var hailstones = Calculator.Calculation(y);
+
+            Console.WriteLine("Output hailstones: ");
+            foreach (long i in hailstones)
+            {
+                Console.WriteLine($"    {(hailstones.IndexOf(i)) + 1}:    {i} ");
+            }
+
+            Console.WriteLine($"Count hailstones: {hailstones.Count}");
+        }
+    }
+}
